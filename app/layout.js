1063f58606e89613aea12dc1852cfc24cc3fd582
PupaLover <lukicevgeorgij@gmail.com>
2025-02-11 16:20:38 +0300

    abobas

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// import { TelegramWebApps } from 'telegram-webapps-types';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,11 +15,16 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "GafkoWEB 0.0.0a",
   description: "Biggest misstake of all Earth",
+
 };
  
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script src="https://telegram.org/js/telegram-web-app.js"></script>
+      </head>
+
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
